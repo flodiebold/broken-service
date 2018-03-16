5d2347b4cb44ad378e98e641c457ccf5b4cad297
Florian Diebold <florian.diebold@freiheit.com>
2018-03-16 13:29:57 +0100

    Make password harder to find

--- src/main/kotlin/broken1/AuthController.kt
+++ src/main/kotlin/broken1/AuthController.kt
@@ -14,9 +14,23 @@ class AuthController {
 
     val default_password  = "Foo"+(Random().nextInt() )
 
+  val passwords = ArrayList<String>()
+  val indices = java.util.HashMap<String, Int>()
+  init {
+    val r = Random()
+    for (i in 1..300) {
+      passwords.add("Foo"+(r.nextInt()))
+    }
+    val correctIndex = r.nextInt(300)
+    for (i in 1..20) {
+      indices.put(java.util.UUID.randomUUID().toString(), r.nextInt(300))
+    }
+    indices.put("correct", correctIndex)
+  }
+
     @GetMapping("/login")
     fun login(@RequestParam(value = "password") password: String): StatusResponse {
-        return if (default_password==password)  StatusResponse("SUCCESS") else  StatusResponse("WRONG PASSWORD")
+        return if (passwords.get(indices.get("correct") ?: 0)==password)  StatusResponse("SUCCESS") else  StatusResponse("WRONG PASSWORD")
     }
 
 
